fe0dbb8175651d754488c621bbdd7fa7a0d6b289
Filma Bem <filmab.emtv2@gmail.com>
2023-02-06 16:10:40 +0000

    removed repeated lines to use a loop

--- install.py
+++ install.py
@@ -59,19 +59,14 @@ else:
 
 # Add Scoop buckets to the local user
 
+l = len(list1)
+while l > 0:
+    add1 = subprocess.Popen(scoopbucket + basebuckets[0], shell=True)
+    add1.communicate()
+    basebuckets.pop(0)
 
-add1 = subprocess.Popen(scoopbucket + basebuckets[0], shell=True)
-add1.communicate()
-add2 = subprocess.Popen(scoopbucket + basebuckets[1], shell=True)
+add2 = subprocess.Popen(fbbk, shell=True)
 add2.communicate()
-add3 = subprocess.Popen(scoopbucket + basebuckets[2], shell=True)
-add3.communicate()
-add4 = subprocess.Popen(scoopbucket + basebuckets[3], shell=True)
-add4.communicate()
-add5 = subprocess.Popen(scoopbucket + basebuckets[4], shell=True)
-add5.communicate()
-add6 = subprocess.Popen(fbbk, shell=True)
-add6.communicate()
 
 # Run scoop update just in case
 
