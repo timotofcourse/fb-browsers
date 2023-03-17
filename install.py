950e0ba50f666ae73312f55eba0b806f8844a5d5
Filma Bem <filmab.emtv2@gmail.com>
2023-03-17 11:41:35 +0000

    Changed the back-end from scoop to winget

--- install.py
+++ install.py
@@ -1,5 +1,3 @@
-import time
-import subprocess
 import os
 import tkinter # In some code editors it may seem tkinter is not used but it's required to import it to use customtkinter
 import customtkinter
@@ -14,95 +12,34 @@ app.title('FB Browsers') # Window Title
 app.iconbitmap('icon.ico') # Window Icon
 app.resizable(False, False)
 
-# Some Variables
-
 home = os.path.expanduser('~')
-scoopfolder = home + "/scoop/apps/scoop"
-scoopinstalled = os.path.exists(scoopfolder)
-gitinstall = 'powershell scoop install git'
-scoopbucket = 'powershell scoop bucket add '
-basebuckets = ['main', 'extras', 'nonportable', 'java', 'versions']
-fbbk = scoopbucket + 'filmabem https://github.com/FilmaBem2/applications.git'
-
-# Install Scoop to install the browser
-
-if scoopinstalled == True:
-    first = subprocess.Popen(gitinstall, shell=True)
-    first.communicate()
-else:
-    first = subprocess.Popen('powershell irm get.scoop.sh | iex')
-    first.communicate()
-    if first.returncode != 0:
-        
-        #  Create a new window
-        
-        toplevel = customtkinter.CTkToplevel()
-        toplevel.geometry("400x200")
-        
-        # Code for the "OK" button
-        
-        def leave():
-            toplevel.destroy()
-        
-        # Error message and ok button
-        
-        errorlabel = customtkinter.CTkLabel(toplevel, text="We could not install scoop automatically. \nYou will have to do it manuall by using this command\n\"irm get.scoop.sh | iex\"")
-        errorlabel.pack()
-        okbtn = customtkinter.CTkButton(toplevel, text="OK", command=leave)
-        okbtn.pack()
-
-    else:
-        time.sleep(0)
-    second = subprocess.Popen(gitinstall, shell=True)
-    second.communicate()
-
-# Add Scoop buckets to the local user
-
-
-while len(basebuckets) > 0:
-    addbuckets = subprocess.Popen(scoopbucket + basebuckets[0], shell=True)
-    addbuckets.communicate()
-    basebuckets.pop(0)
-
-addmybucket = subprocess.Popen(fbbk, shell=True)
-addmybucket.communicate()
-
-# Run scoop update just in case
-
-update = subprocess.Popen('powershell scoop update', shell=True)
-update.communicate()
 
 # Functions to install the browsers
 
 def installbrave():
-    bravevar = 'powershell scoop install brave'
-    braveinstall = subprocess.Popen(bravevar, shell=True)
-    braveinstall.communicate()
+    brave = 'winget install -e --id Brave.Brave'
+    os.system(brave)
 
 def installchrome():
-    chromevar = 'powershell scoop install googlechrome'
-    chromeinstall = subprocess.Popen(chromevar, shell=True)
-    chromeinstall.communicate()
+    chrome = 'winget install -e --id Brave.Brave'
+    os.system(chrome)
 
 def installfirefox():
-    firefoxvar = 'powershell scoop install firefox'
-    firefoxinstall = subprocess.Popen(firefoxvar, shell=True)
-    firefoxinstall.communicate()
+    firefox = 'winget install -e --id Mozilla.Firefox'
+    os.system(firefox)
 
 def installopera():
-    operavar = 'powershell scoop install opera'
-    operainstall = subprocess.Popen(operavar, shell=True)
-    operainstall.communicate()
+    opera = 'winget install -e --id Opera.Opera'
+    os.system(opera)
 
 def installoperagx():
-    operagxvar = 'powershell scoop install operagx'
-    operagxinstall = subprocess.Popen(operagxvar, shell=True)
-    operagxinstall.communicate()
+    operagx = 'winget install -e --id Opera.OperaGX'
+    os.system(operagx)
 
 def installvivaldi():
-    vivaldivar = 'powershell scoop install vivaldi'
-    vivaldiinstall = subprocess.Popen(vivaldivar, shell=True)
-    vivaldiinstall.communicate()
+    vivaldi = 'winget install -e --id VivaldiTechnologies.Vivaldi'
+    os.system(vivaldi)
+
 
 # UI Widgets
 
